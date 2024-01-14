d106f98b4d6c080d09c7379a09b3c0b33b1003f6
Ekgzl <105228606+Ekgzl@users.noreply.github.com>
2024-01-14 01:15:55 +0100

    Program

---
+++ Calculator.cs
@@ -0,0 +1,21 @@
+﻿namespace FirstCalculator_;
+public class Calculator
+{
+    public float Sum(float a, float b)
+    {
+        return a + b;
+    }
+
+    public float Divide(float a, float b)
+    {
+        return a / b;
+    }
+    public float Multiplication(float a, float b)
+    {
+        return a * b;
+    }
+    public float Substract(float a, float b)
+    {
+        return a - b;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿namespace FirstCalculator_;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator main = new Calculator();
+        Console.WriteLine("Please enter first number: ");
+        float[] array = new float[2];
+        array[0] = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Please enter second number: ");
+        array[1] = Convert.ToInt32(Console.ReadLine());
+
+        bool flag = false;
+        bool flag2 = true;
+        do
+        {
+            Console.WriteLine("Please enter a operator: ");
+            char keySwitch = Console.ReadLine()[0];
+            switch (keySwitch)
+            {
+                case '+':
+                    Console.WriteLine(main.Sum(array[0], array[1]));
+                    
+                    break;
+                case '-':
+                    Console.WriteLine(main.Substract(array[0], array[1]));
+                    flag = false;
+                    break;
+                case '/':
+                    Console.WriteLine(main.Divide(array[0], array[1]));
+                    flag = false;
+                    break;
+                case '*':
+                    Console.WriteLine(main.Multiplication(array[0], array[1]));
+                    flag = false;
+                    break;
+                default:
+                    Console.WriteLine("ERROR!");
+                    flag = true;
+                    break;
+            }
+
+            if (flag == false)
+            {
+                char secondKey;
+                do
+                {
+                    Console.WriteLine("Do you want to play again?[Y]/[N] : ");
+                    secondKey = Console.ReadLine()[0];
+                } while (secondKey != 'Y' && secondKey != 'y'&& secondKey!= 'N'&& secondKey!='n');
+
+                if (secondKey == 'y' || secondKey == 'Y')
+                {
+                    flag2 = true;
+                    flag = true;
+                }
+                    
+                else
+                {
+                    flag2 = false;
+                }
+            }
+        } while (flag && flag2);
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadLine();
+    }
+}
